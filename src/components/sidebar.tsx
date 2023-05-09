cd4ca4a5f1579a25b8a703a39b0832b1fae24f30
朱恒霖 <zhuhenglin21@gmail.com>
2023-05-09 19:43:18 +0800

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
       <div className="lg:sticky lg:top-20">
         <LayoutGroup>
           <nav
-            className="flex flex-row md:flex-col mb-3 items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+            className="flex flex-row md:flex-col mb-3 items-start relative px-4 md:px-0 pb-0 fade"
             id="nav"
           >
             <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
