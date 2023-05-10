a83af75a288bbb1119f75ef9e732545b6f4fb9cf
朱恒霖 <zhuhenglin21@gmail.com>
2023-05-10 21:19:37 +0800

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { LayoutGroup, motion } from 'framer-motion';
+import { LayoutGroup } from 'framer-motion';
 import cx from 'classnames';
 
 const navItems = {
