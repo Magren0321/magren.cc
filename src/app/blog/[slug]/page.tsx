8ddfa7f074594c4652a45147aeb0a090b3a22a06
Magren <zhuhenglin21@gmail.com>
2023-09-04 11:57:43 +0800

    feat: add waline

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,10 +2,11 @@ import { getPageData } from '@/lib/notion'
 import NotionContent from '@/components/NotionContent'
 import { Metadata } from "next"
 import Link from 'next/link'
+import Waline from '@/components/Waline'
 
 export default async function Page({params}:{params:{slug: string}}) {
   const {page,blockResults} = await getPageData(params.slug)
-  
+
   return (
     <section>
       <p className='text-4xl font-bold mb-5'>{page?.pageTitle}</p>
@@ -19,6 +20,20 @@ export default async function Page({params}:{params:{slug: string}}) {
       <NotionContent blocks={blockResults}/>
       <div className='mt-10 font-mono opacity-50 hover:opacity-75'>
         <Link href={'/blog'}>{'>'}<span className='border-solid border-b-2 border-b-[#000] ml-2 dark:border-b-[#fff]'>cd . . </span></Link>
+      </div>
+      <div className='mt-10'>
+        <Waline
+          serverURL='https://waline.magren.cc'
+          path={'/' + params.slug}
+          emoji={[
+            '//cdn.jsdelivr.net/gh/walinejs/emojis@1.1.0/tw-emoji'
+          ]}
+          dark={'auto'}
+          meta={['nick', 'mail']}
+          requiredMeta={['nick', 'mail']}
+          imageUploader={false}
+          copyright={false}
+        />
       </div>
     </section>
   )
